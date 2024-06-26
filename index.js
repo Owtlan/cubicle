797e86f367da2e1abceb9f3ce55ec0fb3beda221
Tsvetomir Belchev <99121376+Owtlan@users.noreply.github.com>
2024-06-26 18:06:02 +0300

    new updates on cubicle

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
-const express = require('express');
-
 const config = require('./config/config')
+const express = require('express');
+const { engine } = require('express-handlebars');
+
 
 const app = express()
 
+app.engine('hbs', engine({ extname: '.hbs' }));
 
+app.set('view engine', 'hbs');
+
+app.use(express.static('static'))
+
+
+app.get('/', (req, res) => {
+    res.render('home', { layout: false })
+})
 
 app.listen(config.PORT, () => console.log(`Server is running on port ${config.PORT}...`))
